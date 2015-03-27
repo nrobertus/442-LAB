d83465db550d31a692e094e80edf717b358057b4
Nathan Robertus <nathan.robertus@gmail.com>
2015-03-26 22:22:48 -0600

    Got OpenNi working with the new project

--- OpenCV_REDUX/OpenCV_REDUX/OpenVC_REDUX/Source.cpp
+++ OpenCV_REDUX/OpenCV_REDUX/OpenVC_REDUX/Source.cpp
@@ -3,56 +3,106 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <tchar.h>
+#include "OpenNI.h"
+#include "iostream"
 #include <opencv2/core/core.hpp> // Basic OpenCV structures (cv::Mat, Scalar)
 #include <opencv2/highgui/highgui.hpp> 
-#include <opencv2/imgproc/imgproc.hpp> 
-using namespace cv;
-using namespace std;
+#include <opencv2/imgproc/imgproc.hpp>
+//#include "NiTE.h"
 
 
-int main(int argc, char* argv[])
+int _tmain(int argc, _TCHAR* argv[])
 {
 
+	int c = 100;
+	openni::Status rc = openni::STATUS_OK;
+	openni::Device device;
+	openni::VideoStream depth, color;
+	openni::VideoFrameRef pFrame, dep;
 
-	VideoCapture cap(1);
-	Mat frame1;
-	cap.read(frame1);
-	Mat frame;
-	Mat original;
+	const char* deviceURI = openni::ANY_DEVICE;
 
-	Mat gray = cv::Mat(frame1.size(), CV_8UC1);
-	Mat color = cv::Mat(frame1.size(), CV_32FC3);
-	Mat image1 = frame1.clone();
-	Mat diff = cv::Mat(frame1.size(), CV_8UC3);
+	rc = openni::OpenNI::initialize();
 
-	frame1.convertTo(color, CV_32F);
-	frame.copyTo(image1);
-	frame.copyTo(diff);
+	printf("After initialization:\n%s\n", openni::OpenNI::getExtendedError());
 
-	while (1){
+	rc = device.open(deviceURI);
+	if (rc != openni::STATUS_OK)
+	{
 
-		cap.read(frame);
-		cap.read(original);
+		int a = 0;
+		printf("SimpleViewer: Device open failed:\n%s\n", openni::OpenNI::getExtendedError());
+		openni::OpenNI::shutdown();
+		std::cin >> a;
+		return 1;
 
-		Mat HSV;
-		Mat threshold;
-		Mat edges;
-
-		Scalar hsv_min = cvScalar(0, 200, 170, 0);
-		Scalar hsv_max = cvScalar(200, 250, 256, 0);
-		cvtColor(frame, HSV, CV_BGR2HSV);
-		inRange(HSV, hsv_min, hsv_max, threshold);
-		GaussianBlur(threshold, threshold, Size(7, 7), 1.5);
-		Canny(threshold, edges, 10, 350, 3, false);
-
-		imshow("thr", threshold);
-		imshow("original", HSV);
-		imshow("edges", edges);
-
-		moveWindow("original", 100, 150);
-		moveWindow("thr", 800, 150);
-
-
-		waitKey(1);
 	}
+
+	rc = depth.create(device, openni::SENSOR_DEPTH);
+
+	if (rc == openni::STATUS_OK)
+
+	{
+
+		rc = depth.start();
+		if (rc != openni::STATUS_OK)
+		{
+			printf("SimpleViewer: Couldn't start depth stream:\n%s\n", openni::OpenNI::getExtendedError());
+			depth.destroy();
+		}
+	}
+	else
+	{
+		printf("SimpleViewer: Couldn't find depth stream:\n%s\n", openni::OpenNI::getExtendedError());
+	}
+
+	rc = color.create(device, openni::SENSOR_COLOR);
+	if (rc == openni::STATUS_OK)
+	{
+		rc = color.start();
+		if (rc != openni::STATUS_OK)
+		{
+			printf("SimpleViewer: Couldn't start color stream:\n%s\n", openni::OpenNI::getExtendedError());
+			color.destroy();
+		}
+	}
+	else
+	{
+		printf("SimpleViewer: Couldn't find color stream:\n%s\n", openni::OpenNI::getExtendedError());
+	}
+
+	if (!depth.isValid() || !color.isValid())
+	{
+		printf("SimpleViewer: No valid streams. Exiting\n");
+		openni::OpenNI::shutdown();
+		return 2;
+	}
+	cv::namedWindow("OpenCV", 1);
+	cv::Mat frame = cv::Mat(cv::Size(320, 240), CV_8UC3);
+	//cv::Mat bw = cv::Mat(cv::Size(320,240), CV_16UC1);
+	cv::Mat bw;
+	while (1)
+	{
+		color.readFrame(&pFrame);
+		depth.readFrame(&dep);
+		openni::RGB888Pixel *pColor = (openni::RGB888Pixel *) pFrame.getData();
+		openni::DepthPixel* pDepth = (openni::DepthPixel *) pFrame.getData();
+
+		for (int i = 0; i<frame.rows; i++)
+		{
+			for (int j = 0; j<frame.cols; j++)
+			{
+				openni::RGB888Pixel pix = pColor[frame.cols*i + j];
+				frame.at<cv::Vec3b>(i, j) = cv::Vec3b(pix.r, pix.g, pix.b);
+				bw = cv::Mat(cv::Size(320, 240), CV_16UC1, (void*)pDepth);
+			}
+		}
+
+		cv::imshow("depth", bw);
+		c = cv::waitKey(10);
+		if (c == 27)
+			break;
+	}
+	return 0;
+
 }
